a81c6e39c8fb51842829e66c7df598ad7b49cc4a
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-11-15 14:50:56 -0800

    Found timer variable :D

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Code for program goes here
+import retro
+import tensorflow as tf
+import numpy as np
+
+def get_initial_data():
+    env = retro.make(game="SuperMarioWorld-Snes", state="YoshiIsland1")
+    observations = env.reset()
+    while True:
+        observations, rewards, done, info = env.step(env.action_space.sample())
+        env.render()
+        if done:
+            print("Complete")
+            obs = env.reset()
+    env.close()
+
+get_initial_data()
--- testOpenGym.py
+++ testOpenGym.py
@@ -1,10 +1,3 @@
 import retro
 
-env = retro.make(game="SuperMarioWorld-Snes", state="Bridges1")
-obs = env.reset()
-while True:
-    obs, rew, done, info = env.step(env.action_space.sample())
-    env.render()
-    if done:
-        obs = env.reset()
-env.close()
+
