8911a74cc20820b59c5f83c7b0c1b1df01b0e0b4
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-11-15 15:05:20 -0800

    Update testOpenGym.py

--- testOpenGym.py
+++ testOpenGym.py
@@ -1,3 +1,11 @@
 import retro
 
 
+env = retro.make(game="SuperMarioWorld-Snes", state="Forest1")
+obs = env.reset()
+while True:
+    obs, rew, done, info = env.step(env.action_space.sample())
+    env.render()
+    if done:
+        obs = env.reset()
+env.close()
