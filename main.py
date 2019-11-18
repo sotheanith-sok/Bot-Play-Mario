3ca530dce7212786f680cf18082933d377ec8cd7
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-11-18 14:35:39 -0800

    Add comments and reduce size of model

--- main.py
+++ main.py
@@ -3,50 +3,67 @@ from gym.wrappers import Monitor
 import numpy as np
 from DDQAgent import DDQAgent
 
-
+# Build game enviroment
 env = retro.make(game="SuperMarioWorld-Snes", state="Forest1")
-# env = Monitor(env, './video', force=True, video_callable=lambda episode_id: episode_id%2==0)
+
+# Recoder video for every 10 episodes
+env = Monitor(env, './video', force=True, video_callable=lambda episode_id: episode_id%10==0)
+
+# How many episodes to play
 n_games = 5000
+
+# Initialize double deep q agent
 agent = DDQAgent(
-    alpha=0.0005,
-    gamma=0.99,
-    n_actions=256,
-    epsilon=1.0,
-    batch_size=64,
-    input_dimension=(224, 256, 3),
-    memory_size=1000,
+    alpha=0.0005,       # Learning Rate
+    gamma=0.99,         # Discount factor. Make future event weighted less
+    n_actions=256,      # Number of possible actions. 2^8 for 8 inputs
+    epsilon=1.0,        # How often should agent "explore" (Do random action). Set to 0 for well train model
+    batch_size=64,      # How many samples should this agent train on
+    input_dimension=(224, 256, 3),  # Input dimension.
+    memory_size=1000,   # Max capacity of ReplayBuffer
 )
 
+#Load agent
 agent.load_model()
 
+#Keep track of scores
 scores = []
-render_counter = 10
-learning_counter = 0
-remember_counter = 0
+
+#Some variable
+renderAtEpisode = 10    #At which episode should game be render
+learnEvery = 300        #Keep track of how many frame between each time agent learn
+remeberEvery = 10       #How many frame between each time agent remember
+
+#Start playing
 for i in range(n_games):
-    done = False
+
+    done = False 
     score = 0
-    oberservation = env.reset() / 255.0
+    oberservation = env.reset() / 255.0 #Scale rgb to between 0 and 1
+    frame_counter = 0
+
     while not done:
+        # Render only at target episode
+        # if i % renderAtEpisode == 0:
+        #     env.render()
+
+        # Time Step
         action = agent.choose_action(oberservation)
-        if i < 300 and np.random.random_sample()<0.8:
-            action[7] = 1
         new_oberservation, reward, done, _ = env.step(action)
         new_oberservation = new_oberservation / 255.0
         score += reward
         oberservation = new_oberservation
-
-        if remember_counter % 10 == 0:
+        
+        # Agent only remember 1 frame for every 10 frame
+        if frame_counter % remeberEvery == 0:
             agent.remember(oberservation, action, reward, new_oberservation, done)
 
-        if learning_counter % 300 == 0:
+        # Agent will learn every 300 frame
+        if frame_counter % learnEvery == 0:
             agent.learn()
 
-        # if i % render_counter == 0:
-        #     env.render()
-
-        learning_counter += 1
-
+        frame_counter+=1
+    
     scores.append(score)
     avg_score = np.mean(scores[max(0, i - 100) : i + 1])
     print(
@@ -57,5 +74,6 @@ for i in range(n_games):
         "Epsilon %.2f" % agent.epsilon,
     )
 
+    #Save model every 10 episodes
     if i % 10 == 0 and i > 0:
         agent.save_model()
--- models.py
+++ models.py
@@ -11,13 +11,13 @@ from tensorflow.python.keras.api.keras import (
 
 def build_model(learning_rate, n_actions, input_dimension):
     model = models.Sequential()
-    model.add(layers.Conv2D(64, (3, 3), activation="relu", input_shape=input_dimension))
+    model.add(layers.Conv2D(16, (3, 3), activation="relu", input_shape=input_dimension))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+    model.add(layers.Conv2D(32, (3, 3), activation="relu"))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+    model.add(layers.Conv2D(32, (3, 3), activation="relu"))
     model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation="relu"))
+    model.add(layers.Dense(32, activation="relu"))
     model.add(layers.Dense(n_actions, activation="softmax"))
     model.compile(
         optimizer=optimizers.Adam(learning_rate=learning_rate),
