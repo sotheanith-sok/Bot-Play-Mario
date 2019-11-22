68321b3f312757d54755f18475dff3e00ab7d8da
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-11-22 09:56:29 -0800

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ for i in range(n_games):
         "score: %.8f" % score,
         "Average score: %.8f" % avg_score,
         "Epsilon: %.8f" % agent.epsilon,
-        "Loss: %.8f" % agent.last_loss,
+        "Loss: %.15f" % agent.last_loss,
     )
 
     with open("./data/performance_file.bin", "ab+") as performance_file:
